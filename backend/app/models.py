93b5bb1c2292b3875877d639913ca5712f7176cd
@jonathanlei <jonathanlei98@gmail.com>
2021-12-29 10:38:04 -0800

    added transactionusers table, to_dict to models

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from sqlalchemy.orm import backref
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy_utils import IntRangeType
 
 db = SQLAlchemy()
 
@@ -34,6 +35,7 @@ class User(db.Model, UserMixin):
     updated_at = db.Column(
         db.DateTime, default=lambda: datetime.now(), nullable=False)
     nfts = db.relationship('Nft', backref="user")
+    transactions = db.relationship('TransactionUser', backref="user")
 
     @property
     def password(self):
@@ -45,6 +47,9 @@ class User(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+    def get_nfts(self):
+        return self.nfts
 
     def to_dict(self):
         return {
@@ -68,6 +73,19 @@ class Nft(db.Model):
     owner_id = db.Column(db.Integer, db.ForeignKey(
         "users.id"), nullable=False)
 
+    @property
+    def get_owner(self):
+        return self.owner_id
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "uri": self.uri,
+            "owner": self.owner_id,
+        }
+
 
 class Auction(db.Model):
     """ table for storing NFT auctions """
@@ -79,19 +97,29 @@ class Auction(db.Model):
     end_at = db.Column(db.DateTime(timezone=True),
                        server_default=func.now() + datetime.timedelta(days=1),
                        nullable=False)
-    ## TODO: duration = db.column(db.Interval())
+    # TODO: duration = db.column(db.Interval())
     # timer solution
     nft_id = db.relationship(
         db.Integer, db.ForeignKey("nfts.id"), nullable=False)
     starting_price = db.Column(db.Float, nullable=False)
+    # need to be tested
+    duration = db.Column(IntRangeType)
     current_highest_price = db.Column(db.Float)
-    current_highest_bidder = db.relationship(
-        db.Integer, db.ForeignKey("users.id"))
-    winner = db.relationship(db.Integer, db.ForeignKey("users.id"))
+    current_highest_bidder = db.Column(
+        db.Integer, db.ForeignKeyf("users.id"))
+    winner = db.Colunmn(db.Integer, db.ForeignKey("users.id"))
+    bids = db.relationship("Bid")
+
     # minimum increment
     # TODO: add relationship backrefs for bidders
     # bidders = db.relationship()
     # store all the info about all the bids - history of all the previous bids
+    def check_expiration():
+        return None
+
+    # function to check for expiration, if it's within last 10 mins, a bid is made, then extend by 10 mins
+    def extend_auction(self):
+        return None
 
 
 class AuctionUser(db.model):
@@ -122,6 +150,48 @@ class Bid(db.model):
                          nullable=False)
     bid_amount = db.Column(db.Float, nullable=False)
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "auction_id": self.auction_id,
+            "user_id": self.user_id,
+            "bid_time": self.bid_time,
+            "bid_amount": self.bid_amount,
+        }
+
 
 class Transaction(db.model):
-    """ joint table for auctions and bidders """
+    """ Table for nft transactions"""
+    __tablename__ = "transactions"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nft_id = db.Column(db.Integer, db.ForeignKey("nfts.id"))
+    buyer = db.Column(db.Integer, db.ForeignKey("users.id"))
+    seller = db.Column(db.Integer, db.ForeignKey("users.id"))
+    price = db.Column(db.float, nullable=False, default=0)
+    transaction_time = db.Column(
+        db.DateTime, default=lambda: datetime.now(), nullable=False)
+    auction_id = db.Column(db.Integer, db.ForeignKey(
+        "auctions.id"), nullable=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nft_id": self.nft_id,
+            "buyer": self.buyer,
+            "seller": self.seller,
+            "price": self.price,
+            "transaction_time": self.transaction_time,
+            "auction_id": self.auction_id,
+        }
+
+
+class TransactionUser(db.model):
+    """ Joint Table for nft transactions and users"""
+    __tablename__ = "transactions_users"
+    transaction_id = db.Column(db.Integer,
+                               db.ForeignKey("transactions.id"),
+                               primary_key=True)
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey("users.id"),
+                        primary_key=True)
